5d0fe3817da3c96ad0531d6caefad757ffddb3d1
Aylin Gokalp <aylin@monizze.be>
2023-12-27 19:18:04 +0100

    feat(ui): create component ContentContainer

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,20 @@
 import { Routes } from '@types';
+import { ContentContainer } from '@ui';
 import { getPath } from '@utils';
 import { Link } from 'expo-router';
 import { Text, View } from 'tamagui';
 
 export default function HomeScreen() {
   return (
-    <View alignItems="center" flex={1} justifyContent="center">
-      <Link href={getPath(Routes.RecipeView, { id: '123' })}>
-        <Text>View Recipe 123</Text>
-      </Link>
-      <Link href={getPath(Routes.RecipeEdit, { id: '456' })}>
-        <Text>Edit Recipe 456</Text>
-      </Link>
-    </View>
+    <ContentContainer>
+      <View alignItems="center" justifyContent="center">
+        <Link href={getPath(Routes.RecipeView, { id: '123' })}>
+          <Text>View Recipe 123</Text>
+        </Link>
+        <Link href={getPath(Routes.RecipeEdit, { id: '456' })}>
+          <Text>Edit Recipe 456</Text>
+        </Link>
+      </View>
+    </ContentContainer>
   );
 }
---
+++ src/ui/containers/ContentContainer.tsx
@@ -0,0 +1,17 @@
+import { FC, PropsWithChildren, ReactNode } from 'react';
+import { View, YStack } from 'tamagui';
+
+type ContentContainerProps = PropsWithChildren<{ header?: ReactNode }>;
+
+export const ContentContainer: FC<ContentContainerProps> = ({ children, header }) => {
+  if (header) {
+    return (
+      <YStack marginHorizontal="$4" rowGap="$4">
+        {header}
+        {children}
+      </YStack>
+    );
+  }
+
+  return <View margin="$4">{children}</View>;
+};
