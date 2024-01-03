b4e1127ee35cb514fe4f76cfe4003f5c4bbf7a00
Aylin Gokalp <aylin@monizze.be>
2024-01-03 18:36:30 +0100

    refactor(headers): use dimensions hook instead of class

--- src/components/headers/HomeHeader.tsx
+++ src/components/headers/HomeHeader.tsx
@@ -1,8 +1,7 @@
 import { HeaderTitle } from '@react-navigation/elements';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
-import { Dimensions } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { View, XStack } from 'tamagui';
+import { useWindowDimensions, View, XStack } from 'tamagui';
 
 import { RootStackHeaderRight } from './RootStackHeaderRight';
 
@@ -11,18 +10,15 @@ export const HomeHeader = (props: NativeStackHeaderProps) => {
     options: { title = '' },
   } = props;
   const { top } = useSafeAreaInsets();
+  const { width } = useWindowDimensions();
 
   return (
     <XStack alignItems="center" height={40} marginTop={top}>
-      <View width={Dimensions.get('window').width / 3} />
-      <View width={Dimensions.get('window').width / 3} alignItems="center">
+      <View width={width / 3} />
+      <View width={width / 3} alignItems="center">
         <HeaderTitle>{title}</HeaderTitle>
       </View>
-      <View
-        alignItems="flex-end"
-        justifyContent="center"
-        width={Dimensions.get('window').width / 3}
-      >
+      <View alignItems="flex-end" justifyContent="center" width={width / 3}>
         <RootStackHeaderRight />
       </View>
     </XStack>
